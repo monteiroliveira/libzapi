d9771b676bdd43b1b1ed4077156d238af897fb07
Leandro Meili <leandro.meili@gmail.com>
2025-11-16 17:33:01 -0300

    add and test user segment cmds

---
+++ libzapi/application/commands/help_center/user_segments_cmds.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from typing import Optional, Literal
+
+
+@dataclass(frozen=True, slots=True)
+class BaseUserSegmentCmd:
+    name: str
+    user_type: Literal["signed_in_users", "staff"]
+    tags: Optional[list[str]] = None
+    or_tags: Optional[list[str]] = None
+    added_user_ids: Optional[list[int]] = None
+    groups_ids: Optional[list[int]] = None
+    organization_ids: Optional[list[int]] = None
+
+    def __post_init__(self):
+        if self.user_type not in ("signed_in_users", "staff"):
+            raise ValueError("user_type must be 'signed_in_users' or 'staff'")
+
+
+@dataclass(frozen=True, slots=True)
+class CreateUserSegmentCmd(BaseUserSegmentCmd):
+    ...
+
+
+@dataclass(frozen=True, slots=True)
+class UpdateUserSegmentCmd(BaseUserSegmentCmd):
+    ...
--- tests/unit/help_center/test_user_segment.py
+++ tests/unit/help_center/test_user_segment.py
@@ -1,6 +1,9 @@
+import pytest
+
 from hypothesis.strategies import builds, just
 
 from libzapi.domain.models.help_center.user_segment import UserSegment
+from libzapi.application.commands.help_center.user_segments_cmds import CreateUserSegmentCmd
 from hypothesis import given
 
 strategy = builds(
@@ -12,3 +15,20 @@ strategy = builds(
 @given(strategy)
 def test_session_logical_key_from_id(model: UserSegment) -> None:
     assert model.logical_key.as_str() == "user_segment:cciia"
+
+
+def test_create_user_segment_command_fail():
+    with pytest.raises(ValueError):
+        CreateUserSegmentCmd(
+            name="Test Segment",
+            user_type="no-user"  # Invalid user_type
+        )
+
+
+def test_create_user_segment_command_success():
+    cmd = CreateUserSegmentCmd(
+        name="Test Segment",
+        user_type="signed_in_users"
+    )
+    assert cmd.name == "Test Segment"
+    assert cmd.user_type == "signed_in_users"
