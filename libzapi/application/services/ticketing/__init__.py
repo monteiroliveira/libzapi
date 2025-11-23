8760ea1feba1c5a3baa84690133208c9869e482a
Leandro Meili <leandro.meili@gmail.com>
2025-11-23 00:29:40 -0300

    add ticket getters

--- libzapi/application/services/ticketing/__init__.py
+++ libzapi/application/services/ticketing/__init__.py
@@ -10,6 +10,7 @@ from libzapi.application.services.ticketing.schedule_service import ScheduleServ
 from libzapi.application.services.ticketing.sessions_service import SessionsService
 from libzapi.application.services.ticketing.sla_policies_service import SlaPoliciesService
 from libzapi.application.services.ticketing.support_addresses_service import SupportAddressesService
+from libzapi.application.services.ticketing.tickets_service import TickestService
 from libzapi.application.services.ticketing.ticket_audits_service import TicketAuditsService
 from libzapi.application.services.ticketing.ticket_fields_service import TicketFieldsService
 from libzapi.application.services.ticketing.ticket_forms_service import TicketFormsService
@@ -48,6 +49,7 @@ class Ticketing:
         self.sessions = SessionsService(api.SessionApiClient(http))
         self.sla_policies = SlaPoliciesService(api.SlaPolicyApiClient(http))
         self.support_addresses = SupportAddressesService(api.SupportAddressApiClient(http))
+        self.tickets = TickestService(api.TicketApiClient(http))
         self.ticket_audits = TicketAuditsService(api.TicketAuditApiClient(http))
         self.ticket_fields = TicketFieldsService(api.TicketFieldApiClient(http))
         self.ticket_forms = TicketFormsService(api.TicketFormApiClient(http))
